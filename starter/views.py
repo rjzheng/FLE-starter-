a35778bf0cfa405e3cd72e1fc9637f7c02babe55
rjzheng <rjzheng@ucsd.edu>
2014-12-09 18:28:52 -0800

    html layout

--- starter/views.py
+++ starter/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 import json
 
 def landing_page(request):
@@ -18,6 +19,9 @@ def json_read(request):
 	release_dates_list = []
 	ratings_list = []
 	synopsis_list = []
+	critics_rating_list = []
+	critics_score_list = []
+	audience_score_list = []	
 	actors_list = []
 	characters_list = []
 	posters_list = []
@@ -27,10 +31,17 @@ def json_read(request):
 		mpaa_rating_list.append(values['movies'][x]['mpaa_rating'])
 		runtime_list.append(values['movies'][x]['runtime'])
 		release_dates_list.append(values['movies'][x]['release_dates']['theater'])
-		ratings_list.append(values['movies'][x]['ratings'])
+		critics_score_list.append(values['movies'][x]['ratings']['critics_score'])
+		audience_score_list.append(values['movies'][x]['ratings']['audience_score'])
 		synopsis_list.append(values['movies'][x]['synopsis'])
-		actors_list.append(values['movies'][x]['abridged_cast'][0]['name'] + "   ")
-		#characters_list.append(values['movies'][x]['abridged_cast']['characters'])
+		#actors_list.append(values['movies'][x]['abridged_cast'][0]['name'] + "   ")
+		#characters_list.append(values['movies'][x]['abridged_cast'][0]['characters'])
 		posters_list.append(values['movies'][x]['posters']['thumbnail'])
 	#return HttpResponse(json.dumps(title_list, indent=1))
-	return HttpResponse(actors_list)
+	return render_to_response("starter/base.html", {'title': title_list[0], 'poster': posters_list[0],
+	 						  'year': year_list[0], 'ratings': mpaa_rating_list[0],
+	 						  'runtime': runtime_list[0],'critics_score': critics_score_list[0],
+	 						  'audience_score': audience_score_list[0], 'release_dates': release_dates_list[0],
+	 						  'synopsis': synopsis_list[0]})
+
+
